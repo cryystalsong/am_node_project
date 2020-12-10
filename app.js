e2a85cfd3dd40491440108ae791de10e9a950639
crys <cryystalsong@gmail.com>
2020-12-09 19:15:09 -0800

    add pug layout that contains navbar, styles
    navbar active tab is rendered dynamically by checking input path

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set('views', 'views/pug-views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404');
+    res.status(404).render('404',{pageTitle: "Page Not Found"});
 });
 
 app.listen(4000);
