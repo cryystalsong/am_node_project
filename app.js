c4aed30dda6c6896602aad1145e0493767773a55
Crystal <cryystalsong@gmail.com>
2020-12-09 22:04:05 -0800

    convert to ejs

--- app.js
+++ app.js
@@ -2,22 +2,27 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHBs = require('express-handlebars');
+// const expressHBs = require('express-handlebars');
 
 const app = express();
 
-// express handle bars is not built in, so must explicitly register it w/ node
-app.engine(
-    'hbs', //'hbs' is arbitrarily given here, also becomes the file type .hbs 
-    expressHBs({ 
-        layoutsDir: 'views/hbs-views/layouts', 
-        defaultLayout: 'main-layout',
-        extname: 'hbs'
-    })
-); 
-app.set('view engine', 'hbs');
-app.set('views', 'views/hbs-views');
+app.set('view engine', 'ejs');
+app.set('views', 'views/ejs-views');
 
+/* HANDLEBARS 
+express handle bars is not built in, so must explicitly register it w/ node */
+// app.engine(
+//     'hbs', //'hbs' is arbitrarily given here, also becomes the file type .hbs 
+//     expressHBs({ 
+//         layoutsDir: 'views/hbs-views/layouts', 
+//         defaultLayout: 'main-layout',
+//         extname: 'hbs'
+//     })
+// ); 
+// app.set('view engine', 'hbs');
+// app.set('views', 'views/hbs-views');
+
+// PUG
 // app.set('view engine', 'pug');
 // app.set('views', 'views/pug-views');
 
