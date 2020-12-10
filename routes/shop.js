f3642e656be41f17a039af72f3171d654b2718cb
crys <cryystalsong@gmail.com>
2020-12-09 18:27:10 -0800

    set view engine to pug, and created pug version of `shop.html`

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,12 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log('shop.js', adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+  // defined 'pug' as the default render engine, 
+  // also no need to define path to 'shop' since we already previously defined views to be within the dir `view`
+  res.render('shop'); 
 });
 
 module.exports = router;
